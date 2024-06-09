a2509b6aa4dbfb43443b6b28ef2ddf4f6ec07668
ldk0225 <ldk0225@midasin.com>
2024-06-09 14:56:00 +0900

    6. Use

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,10 @@
-import { useOptimistic, useState, useRef } from "react";
 
 /**
- * https://react.dev/reference/react/useOptimistic
+ * https://react.dev/reference/react/use
  */
 
-export async function deliverMessage(message) {
-    await new Promise((res) => setTimeout(res, 1000));
-    return message;
+function App() {
+    return <></>
 }
 
-
-function Thread({ messages, sendMessage }) {
-    const formRef = useRef();
-    async function formAction(formData) {
-        addOptimisticMessage(formData.get("message"));
-        formRef.current.reset();
-        await sendMessage(formData);
-    }
-    const [optimisticMessages, addOptimisticMessage] = useOptimistic(
-        messages,
-        (state, newMessage) => [
-            ...state,
-            {
-                text: newMessage,
-                sending: true
-            }
-        ]
-    );
-
-    return (
-        <>
-            {optimisticMessages.map((message, index) => (
-                <div key={index}>
-                    {message.text}
-                    {!!message.sending && <small> (Sending...)</small>}
-                </div>
-            ))}
-            <form action={formAction} ref={formRef}>
-                <input type="text" name="message" placeholder="Hello!" />
-                <button type="submit">Send</button>
-            </form>
-        </>
-    );
-}
-
-export default function App() {
-    const [messages, setMessages] = useState([
-        { text: "Hello there!", sending: false, key: 1 }
-    ]);
-    async function sendMessage(formData) {
-        const sentMessage = await deliverMessage(formData.get("message"));
-        setMessages((messages) => [...messages, { text: sentMessage }]);
-    }
-    return <Thread messages={messages} sendMessage={sendMessage} />;
-}
+export default App
